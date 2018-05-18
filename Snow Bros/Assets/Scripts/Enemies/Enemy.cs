ac0cbd0f673f36afb28190481c80f4469d283b94
buihuyphuc <buihuyphuc97@gmail.com>
2018-05-18 21:59:00 +0700

    k biet toi dau roi

--- Snow Bros/Assets/Scripts/Enemies/Enemy.cs
+++ Snow Bros/Assets/Scripts/Enemies/Enemy.cs
@@ -13,9 +13,11 @@ public class Enemy : MonoBehaviour {
     [SerializeField]
     private PhysicsMaterial2D material;
     private bool collision, ground = true, isFreeze = false, attack = false;
+    private bool flyAI = false; //xử lý việc AI của enemy khi rơi
     private bool bulleting = false; //xử lý việc chỉ cho enemy bắn 1 viên đạn 1 lần
-    private float timeDirection = 0, timeJump = 0, timeFreeze = 0, timeAttack = 0;
+    private float timeAI = 0, timeFreeze = 0, timeAttack = 0;
     private int lvFreeze = 0, damage = 0;
+    private float timeRdAI = 3f;
 
     public float speed = 7f;
     public float forceY = 2000f;
@@ -24,12 +26,14 @@ public class Enemy : MonoBehaviour {
     private Rigidbody2D myBody;
     private Animator anim;
     private CircleCollider2D cirCollider;
+    private Transform playerTranform;
 
     private void Awake()
     {
         myBody = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         cirCollider = GetComponent<CircleCollider2D>();
+        playerTranform = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
     }
 
     // Use this for initialization
@@ -47,17 +51,26 @@ public class Enemy : MonoBehaviour {
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Ground")
+        if(collision.gameObject.tag == "Ground" && isLive)
         {
             ground = true;
             anim.SetBool("Fly", false);
             if (myBody.velocity.y <= 0)
             {
-                timeDirection = 0;
                 anim.SetBool("Jump", false);
             }
         }
-        if(collision.gameObject.tag == "Enemy")
+        else if(collision.gameObject.tag == "Ground" && !isLive)
+        {
+            anim.SetBool("Die2", true);
+            Destroy(gameObject, .5f);
+        }
+        if (collision.gameObject.tag == "Tuong")
+        {
+            if(flyAI || !sizeJump.GetComponent<SizeJumpEnemy>().sizeJump)
+                ChangeDirection();//khi enemy dung cao hon player se tim noi de nhay xuong neu dung phai tuong se quay dau
+        }
+        if (collision.gameObject.tag == "Enemy")
         {
             myBody.gravityScale = 0;
             cirCollider.isTrigger = true;
@@ -71,8 +84,21 @@ public class Enemy : MonoBehaviour {
             if (!anim.GetBool("Jump") && myBody.velocity.y < -3f)
             {
                 ground = false;
+                if (flyAI)
+                {
+                    timeAI = 0;
+                }
                 anim.SetBool("Fly", true);
             }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Die" && !isLive)
+        {
+            cirCollider.isTrigger = false;
+            myBody.freezeRotation = true;
         }
     }
 
@@ -183,8 +209,7 @@ public class Enemy : MonoBehaviour {
     {
         if (!isFreeze)
         {
-            timeDirection += Time.deltaTime;
-            timeJump += Time.deltaTime;
+            timeAI += Time.deltaTime;
             if (bullet != null && bulletPos != null)
             {
                 timeAttack += Time.deltaTime;
@@ -194,15 +219,33 @@ public class Enemy : MonoBehaviour {
                 if (!attack)
                 {
                     Move();
-                    if (timeJump > Random.Range(1f, 5f) && sizeJump.GetComponent<SizeJumpEnemy>().sizeJump)
+                    if (timeAI > .5f)
                     {
-                        Jump();
-                        timeJump = 0;
+                        anim.SetBool("Roll", false);
                     }
-                    else if (timeDirection > .1f && timeDirection < Random.Range(3f, 10f))
+                    if (timeAI > timeRdAI)
+                    {
+                        float sizeY = transform.position.y - playerTranform.position.y;
+                        if (sizeY < -1f && sizeJump.GetComponent<SizeJumpEnemy>().sizeJump)
+                        {
+                            timeAI = 0;
+                            Jump();
+                        }
+                        else if (sizeY > -1f && sizeY < 1f)
+                        {
+                            timeAI = 0;
+                            anim.SetBool("Roll", true);
+                        }
+                        else if (sizeY > 1f)
+                        {
+                            flyAI = true;
+                        }
+                    }
+                    else if(!flyAI)
                     {
                         ChangeDirection();
                     }
+
                     if (timeAttack > Random.Range(10f, 15f))
                     {
                         attack = true;
@@ -234,5 +277,15 @@ public class Enemy : MonoBehaviour {
                 }
             }
         }
+    }
+
+    public void Die()
+    {
+        isLive = false;
+        cirCollider.isTrigger = true;
+        myBody.freezeRotation = false;
+        myBody.gravityScale = 10;
+        myBody.AddForce(new Vector2(Random.Range(-500, 500f), forceY));
+        anim.SetBool("Die1", true);
     }
 }
