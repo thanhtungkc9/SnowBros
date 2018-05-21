7763fc177e18125477bc11a0b6dce07447bdd26a
Thanh Tung <thanhtung.k10uit@gmail.com>
2018-05-22 02:05:07 +0700

    Chinh sua chuyen doi animation player

--- Snow Bros/Assets/Scripts/Player/Bullet.cs
+++ Snow Bros/Assets/Scripts/Player/Bullet.cs
@@ -6,13 +6,18 @@ public class Bullet : MonoBehaviour {
 
     public float speed = 10f;
     public int dmg = 10;
+    public float forceX=400f;
+    public float forceY=-150f;
+
 
     private bool direction;
     private GameObject player;
+    private Rigidbody2D bullet;
 
     private void Awake()
     {
         player = GameObject.Find("Player");
+        bullet = GetComponent<Rigidbody2D>();
     }
 
     // Use this for initialization
@@ -20,10 +25,21 @@ public class Bullet : MonoBehaviour {
         if (player.transform.localScale.x == 1f)
         {
             direction = true;
+            Vector3 scale = transform.localScale;
+            scale.x = 1f;
+            transform.localScale = scale;
+            
         }
         else if (player.transform.localScale.x == -1f)
         {
             direction = false;
+            Vector3 scale = transform.localScale;
+            scale.x = -1f;
+            transform.localScale = scale;
+            ;
+            //bullet.AddForce(new Vector2(-forceX, forceY));
+          //  bullet.velocity = new Vector2(-1f, 0f);
+
         }
     }
 	
@@ -36,26 +52,22 @@ public class Bullet : MonoBehaviour {
     {
         if (direction)
         {
-            Vector3 scale = transform.localScale;
-            scale.x = 1f;
-            transform.localScale = scale;
+           
             Vector2 newPos1 = transform.position;
             newPos1 = new Vector2(newPos1.x + speed * Time.deltaTime, newPos1.y);
             transform.position = newPos1;
         }
         else
         {
-            Vector3 scale = transform.localScale;
-            scale.x = -1f;
-            transform.localScale = scale;
+           
             Vector2 newPos2 = transform.position;
             newPos2 = new Vector2(newPos2.x - speed * Time.deltaTime, newPos2.y);
             transform.position = newPos2;
         }
         yield return new WaitForSeconds(.2f);
-        Vector2 newPos = transform.position;
-        newPos = new Vector2(newPos.x, newPos.y - speed * Time.deltaTime);
-        transform.position = newPos;
+       // Vector2 newPos = transform.position;
+      //  newPos = new Vector2(newPos.x, newPos.y - speed * Time.deltaTime);
+       // transform.position = newPos;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
--- Snow Bros/Assets/Scripts/Player/PlayerScript.cs
+++ Snow Bros/Assets/Scripts/Player/PlayerScript.cs
@@ -17,9 +17,8 @@ public class PlayerScript : MonoBehaviour {
     public const int STATE_DIE = 9;
     public const int STATE_RESPAWN = 10;
     public const int STATE_RUNTHROW = 11;
-    public int CURRENTSTATE = 0;
 
-    private Rigidbody2D playerBody;
+    public Rigidbody2D playerBody;
     private Animator playerAnimator;
     public Transform shootPoint;
     [SerializeField]
@@ -30,8 +29,8 @@ public class PlayerScript : MonoBehaviour {
     public float jumpForce = 700f;
     public float moveXVelocity = 100f;
     public float moveForce = 150f;
-    public float maxVelocity = 8f;
-    private bool grounded, pushed, bullet = false, keyupJ = true, keyupK = true;
+    public float maxVelocity = 3f;
+    public bool grounded, bullet = false, keyupJ = true, keyupK = true;
     void Awake()
     {
         playerBody = GetComponent<Rigidbody2D>();
@@ -42,27 +41,10 @@ public class PlayerScript : MonoBehaviour {
     void Start() {
         shootPoint = transform.Find("ShootPoint");
     }
-
     // Update is called once per frame
     void Update() {
         Keyboard_Move();
-        switch (CURRENTSTATE)
-        {
-            case STATE_IDLE:
-                Idle();
-                break;
-            case STATE_WALK:
-            case STATE_PUSH:
-                Walk_Push();
-                break;
-            case STATE_THROW:
-            case STATE_RUNTHROW:
-                Throw_RunThrow();
-                break;
-            case STATE_JUMP:
-                Jump();
-                break;
-        }
+       
     }
 
     void Keyboard()
@@ -102,73 +84,42 @@ public class PlayerScript : MonoBehaviour {
     void OnCollisionEnter2D(Collision2D target)
     {
 
-        if (target.gameObject.tag == "SnowBall" && CURRENTSTATE != STATE_PUSH && CURRENTSTATE != STATE_IDLE)
+        if (target.gameObject.tag == "Freeze4" && playerAnimator.GetInteger("CurrentState") != STATE_PUSH && playerBody.velocity.x!=0)
         {
-            Debug.Log("Collision with Snowball");
             playerAnimator.SetInteger("CurrentState", STATE_PUSH);
-            CURRENTSTATE = STATE_PUSH;
         }
-        if (target.gameObject.tag == "Ground" || target.gameObject.tag == "Freeze")
+        if ((target.gameObject.tag == "Ground" || target.gameObject.tag == "Freeze4")&&playerBody.velocity.y <= 0&&grounded==false)
         {
             grounded = true;
+
+            Debug.Log("Collision with ground");
             float h = Input.GetAxisRaw("Horizontal");
-            if (h != 0)
+             if (h != 0)
             {
                 playerAnimator.SetInteger("CurrentState", STATE_WALK);
-                CURRENTSTATE = STATE_WALK;
             }
-            else
+            else if (h == 0)
             {
+
                 playerAnimator.SetInteger("CurrentState", STATE_IDLE);
-                CURRENTSTATE = STATE_IDLE;
+
             }
         }
     }
+
     void OnCollisionExit2D(Collision2D target)
     {
-        //xử lý animation fly
+        //
         if (target.gameObject.tag == "Ground" && playerBody.velocity.y < -3f)
         {
             grounded = false;
         }
-
-    }
-    void Idle()
-    {
-        playerBody.velocity = new Vector2(0, playerBody.velocity.y);
-        float h = Input.GetAxisRaw("Horizontal");
-        if (h != 0 && CURRENTSTATE != STATE_WALK)
-        {
-            playerAnimator.SetInteger("CurrentState", 1);
-            CURRENTSTATE = STATE_WALK;
-        }
-
-
-        if (Input.GetKey(KeyCode.J))
-        {
-
-            {
-                playerAnimator.SetInteger("CurrentState", STATE_THROW);
-                CURRENTSTATE = STATE_THROW;
-            }
-
-        }
-        else if (Input.GetKey(KeyCode.K) )
-        {
-            if (grounded && keyupK && playerAnimator.GetInteger("CurrentState")!=STATE_JUMP)
-            {
-                grounded = false;
-                keyupK = false;
-                playerAnimator.SetInteger("CurrentState", STATE_JUMP);
-                CURRENTSTATE = STATE_JUMP;
-            }
-        }
-
     }
 
     void Keyboard_Move()
     {
-        if (CURRENTSTATE == STATE_DIE || CURRENTSTATE == STATE_RESPAWN || CURRENTSTATE == STATE_FLY)
+        int currentState = playerAnimator.GetInteger("CurrentState");
+        if (currentState == STATE_DIE || currentState == STATE_RESPAWN || currentState == STATE_FLY)
             return;
         float h = Input.GetAxisRaw("Horizontal");
         float forceX = 0f;
@@ -216,73 +167,8 @@ public class PlayerScript : MonoBehaviour {
 
     }
 
-    void Jump()
-    {
-        if (keyupK == false)
-        {
-            playerBody.AddForce(new Vector2 (0, jumpForce));
-            keyupK = true;
-
-        }
-
-    }
-    void Walk_Push()
-    {
-
-        float h = Input.GetAxisRaw("Horizontal");
-        if (h > 0)
-        {
-            Vector3 scale = transform.localScale;
-            scale.x = 1f;
-            transform.localScale = scale;
-
-        }
-        else if (h < 0)
-        {
-            Vector3 scale = transform.localScale;
-            scale.x = -1f;
-            transform.localScale = scale;
-        }
-        else if (h == 0)
-            {
-                playerAnimator.SetInteger("CurrentState", 0);
-                CURRENTSTATE = STATE_IDLE;
-            }
-        if (Input.GetKey(KeyCode.J))
-        {
-
-            playerAnimator.SetInteger("CurrentState", STATE_RUNTHROW);
-            CURRENTSTATE = STATE_RUNTHROW;
-        }
-        else if (Input.GetKey(KeyCode.K))
-        {
-
-            if (grounded && keyupK && playerAnimator.GetInteger("CurrentState") != STATE_JUMP)
-            {
-                grounded = false;
-                keyupK = false;
-                playerAnimator.SetInteger("CurrentState", STATE_JUMP);
-                CURRENTSTATE = STATE_JUMP;
-            }
-        }
-    }
-
-    void Throw_RunThrow()
-    {
-        if (keyupJ)
-        {
-            keyupJ = false;
-            playerAnimator.SetBool("Throw", true);
-            StartCoroutine(Attack());
-        }
-        if (Input.GetKeyUp(KeyCode.J))
-        {
-            keyupJ = true;
-            playerAnimator.SetInteger("CurrentState", 0);
-            CURRENTSTATE = STATE_IDLE;
-        }
-    }
-    IEnumerator Attack()
+ 
+    public IEnumerator Attack()
     {
         if (bullet)
         {
@@ -294,7 +180,7 @@ public class PlayerScript : MonoBehaviour {
             bullet = !bullet;
             Instantiate(bullet2, shootPoint.position, Quaternion.identity);
         }
-        yield return new WaitForSeconds(.3f);
+        yield return new WaitForSeconds(.1f);
     }
 
 }
